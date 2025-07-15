fa4703b4dd05889fdd4301ef73cb72fe9c4dcbd2
Yvonne Ntinyari <ntinyariyvonne823@gmail.com>
2025-07-15 09:56:31 +0300

    Fix duplicate useState import

--- app/(sender)/send.tsx
+++ app/(sender)/send.tsx
@@ -1,4 +1,3 @@
-import { useState } from 'react';
 import { useState, useEffect } from 'react';
 import { View, Text, StyleSheet, ScrollView, TextInput, TouchableOpacity, Alert } from 'react-native';
 import { useRouter, useLocalSearchParams } from 'expo-router';
